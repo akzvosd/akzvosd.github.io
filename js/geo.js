4b9e4db3427af59020dde11e304c022113418f6f
obscurya <prostunting@mail.ru>
2020-02-22 21:01:12 +0300

    added geomodal functionality

--- js/geo.js
+++ js/geo.js
@@ -1,45 +1,102 @@
-var apiCity = ymaps.geolocation;
-        var i, ii, delivery;
-        var flag = true;
-        var shopAdress = {
-                "Великий Новгород" : "ТД Русь, 1 этаж",
-                "Санкт-Петербург" : "ТРК Лиговъ, 2 этаж",
-            }
-        var deliveryAdress = {
-                'Абакан' : 'Самовывоз: ул. Чертыгашева, 126, 13 (Бесплатно)',
-                'Абинск' : 'Самовывоз: пр-т Комсомольский, 110“А”/3 (Бесплатно)',
-                'Адлер' : 'Самовывоз: ул. Просвещения, 118б (Бесплатно)',
-                'Азов' : 'Самовывоз: ул. Измайлова, 55 (Бесплатно)',
-                'Аксай' : 'Самовывоз: ул. Садовая, 31 (Бесплатно)',
-                'Алапаевск' : 'Самовывоз: ул. 3-го Интернационала, 22 (Бесплатно)',
-                'Александров' : 'Самовывоз: ул. Институтская, 10 (Бесплатно)',
-                'Алексеевка' : 'Самовывоз: ул. Победы, 51б (Бесплатно)',
-                'Алексин' : 'Самовывоз: ул. Тульская, 8 (Бесплатно)',
-                'Алушта' : 'Самовывоз: ул. Таврическая, 4а (Бесплатно)',
-        }        
-        
-        for (i in shopAdress) {
-          if (apiCity.match(i) == i) {
-          delivery = shopAdress[i]
-          flag = false
-          break
-          } 
-        }
-  
-        if (flag == true) {
-          for (ii in deliveryAdress) {
-            if (apiCity.match(ii) == ii) {
-            delivery = deliveryAdress[ii]
-            flag = false
-            break
-            }
-          }
-        }
-        
-        if (flag == true) {
-            apiCity = 'Бесплатная доставка'
-            delivery = 'по городам России (подробности по телефону)'
-        }
+var tmpHtml = '';
+var abc = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+var cities = {};
+var geo = JSON.parse(localStorage.getItem('geo'));
 
-        $('#town').html(apiCity); 
-        $('#delivery').html(delivery);
+if (geo) {
+  $('#town').html(geo.city);
+  $('#delivery').html(geo.delivery);
+  $('#town--mob').html(geo.city);
+  $('#delivery--mob').html(geo.delivery);
+}
+
+// $(document).ready(function() {
+$(window).on('load', function() {
+  $('#geoModalOpen').click(function() {
+    $('#geoModal').show();
+  });
+  $('#geoModalOpenMobile').click(function() {
+    $('#geoModal').show();
+  });
+  $('#geoModalClose').click(function() {
+    $('#geoModal').hide();
+  });
+
+  if (!geo) changeCity(ymaps.geolocation.city);
+
+  abc.split('').forEach(l => {
+    cities[l] = [];
+  });
+
+  for (let city in deliveryAddress) {
+    let fl = city[0].toLowerCase();
+
+    cities[fl].push(city);
+  }
+
+  tmpHtml = $('.geoModalWindow__footer')[0].innerHTML;
+
+  createGeoModal();
+});
+
+function changeCity(city) {
+  var delivery = shopAddress[city] || deliveryAddress[city] || null;
+
+  if (delivery) {
+    localStorage.setItem('geo', JSON.stringify({ city, delivery }));
+
+    $('#town').html(city);
+    $('#delivery').html(delivery);
+    $('#town--mob').html(city);
+    $('#delivery--mob').html(delivery);
+    $('#geoModal').hide();
+
+    if ($('.geoModalWindow input')[0].value !== '') {
+      $('.geoModalWindow input')[0].value = '';
+
+      createGeoModal();
+    }
+  }
+}
+
+function createGeoModal(search = false, cs = cities) {
+  var html = '';
+
+  if (Object.keys(cs).length) {
+    for (let l in cs) {
+      if (cs[l].length) {
+        html += `<div class="geoModalWindow__footer--wrap"><div class="geoModalWindow__footer--letter">${l.toUpperCase()}</div><div class="geoModalWindow__footer--city">`;
+
+        cs[l].forEach(city => {
+          html += `<p onclick="changeCity('${city}')">${city}</p>`;
+        });
+
+        html += '</div></div>';
+      }
+    }
+  } else {
+    html = '<div class="geoModalWindow__footer--wrap"><div class="geoModalWindow__footer--letter"></div><div class="geoModalWindow__footer--city"><p>Ничего не найдено</p></div></div>';
+  }
+
+  $('.geoModalWindow__footer').html(search ? html : tmpHtml + html);
+}
+
+function handleGeoModalInput(query) {
+  if (query === '') return createGeoModal();
+
+  query = query.toLowerCase();
+
+  let fl = query[0];
+  let array = cities[fl];
+  let cs = {};
+
+  if (array) {
+    cs[fl] = [];
+
+    array.forEach(c => {
+      if (c.toLowerCase().indexOf(query) >= 0) cs[fl].push(c);
+    });
+  }
+
+  createGeoModal(true, cs);
+}
