c6f344ebddeda88209d8a12fd51baf9ee3f718de
akzvosd <32770051+akzvosd@users.noreply.github.com>
2020-07-27 14:40:23 +0300

    imagine-comeback

---
+++ js/counter.js
@@ -0,0 +1,80 @@
+function catalogItemCounter(field) {
+  var fieldCount = function(el) {
+    var // Мин. значение
+      min = el.data('min') || false,
+      // Макс. значение
+      max = el.data('max') || false,
+      // Кнопка уменьшения кол-ва
+      dec = el.prev('.dec'),
+      // Кнопка увеличения кол-ва
+      inc = el.next('.inc');
+
+    function init(el) {
+      if (!el.attr('disabled')) {
+        dec.on('click', decrement);
+        inc.on('click', increment);
+      }
+
+      // Уменьшим значение
+      function decrement() {
+        var value = parseInt(el[0].value);
+        value--;
+
+        if (!min || value >= min) {
+          el[0].value = value;
+
+          setFaces(value);
+        }
+      }
+
+      // Увеличим значение
+      function increment() {
+        var value = parseInt(el[0].value);
+        value++;
+
+        if (!max || value <= max) {
+          el[0].value = value;
+
+          setFaces(value);
+        }
+      }
+    }
+
+    el.each(function() {
+      init($(this));
+    });
+  };
+
+  $(field).each(function() {
+    fieldCount($(this));
+  });
+}
+
+catalogItemCounter('.fieldCount');
+
+var sizePrice = parseInt($('.select--price option').val());
+var facePrice = parseInt($('#facePrice').html());
+var additionalFaces = 0;
+var totalPrice = 0;
+
+calcPrice();
+
+function setSizePrice(e) {
+  sizePrice = parseInt(e.value);
+
+  calcPrice();
+}
+
+function setFaces(value) {
+  additionalFaces = value - 1;
+
+  calcPrice();
+}
+
+function calcPrice() {
+  if (sizePrice === -1) return $('#selectV').html('Для расчёта стоимости <br>нажмите «Сделать заказ»<br> и заполните форму');
+
+  totalPrice = sizePrice + additionalFaces * facePrice;
+
+  $('#selectV').html(`Цена: ${totalPrice} руб.`);
+}
