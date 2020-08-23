446c82b42c9158f0b0481c74638d4d72d44390fb
akzvosd <32770051+akzvosd@users.noreply.github.com>
2020-08-23 15:15:18 +0300

    sasha

--- js/script.js
+++
@@ -1,8 +0,0 @@
-/* overlay menu */
-function openNav() {
-  document.getElementById("myNav").style.height = "100%";
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
-}
